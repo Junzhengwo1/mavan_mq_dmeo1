1de760c3c808e71410becbef992efc61ccdc14a4
俊正我 <69614773+Junzhengwo1@users.noreply.github.com>
2021-02-07 17:27:09 +0800

    ok

--- subscribe_producer/src/main/java/com/kou/service/SendMsg.java
+++ subscribe_producer/src/main/java/com/kou/service/SendMsg.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeoutException;
  */
 public class SendMsg {
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         System.out.println("请输入消息：");
         Scanner scanner=new Scanner(System.in);
         String msg=scanner.nextLine();
@@ -24,8 +24,15 @@ public class SendMsg {
         //创建频道，相当于jdbc的statement
         Channel channel = connection.createChannel();
 
+        //在消息发送之前开启消息确认
+        channel.confirmSelect();
+
         channel.basicPublish("ex1","",null,msg.getBytes());
         System.out.println("send"+msg);
+        //消息发送之后，确认是否成功
+        boolean b = channel.waitForConfirms();
+        System.out.println("发送"+(b?"成功":"失败"));
+
         channel.close();
         connection.close();
     }
